0a8aa6e37be18ec53f4e9a3a2e7efe0e8578e7fd
V.Sergeeva <tishchkan@rambler.ru>
2024-01-03 15:02:06 +0300

    first commit

---
+++ src/test/java/SoftAssertionsTest.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SoftAssertionsTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/selenide/selenide");
+
+
+    }
+}
