c31daeacbeb66156b0b69fa411f9b9e46c936d2b
V.Sergeeva <tishchkan@rambler.ru>
2024-01-03 15:46:43 +0300

    commit

--- src/test/java/SoftAssertionsTest.java
+++ src/test/java/SoftAssertionsTest.java
@@ -2,8 +2,10 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.executeJavaScript;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SoftAssertionsTest {
     @BeforeAll
@@ -18,7 +20,21 @@ public class SoftAssertionsTest {
     @Test
     void fillFormTest() {
         open("/selenide/selenide");
-
-
+        $("#wiki-tab").click();
+        $("#wiki-pages-box").find(byText("Show 3 more pages…")).click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").find(byText("SoftAssertions")).click();
+        $("#user-content-3-using-junit5-extend-test-class").shouldBe(visible);
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
     }
 }
