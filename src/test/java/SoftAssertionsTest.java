fcbbd399aed857e1ca012603bfc5260fd0521e95
V.Sergeeva <tishchkan@rambler.ru>
2024-01-03 15:47:25 +0300

    commit

--- src/test/java/SoftAssertionsTest.java
+++ src/test/java/SoftAssertionsTest.java
@@ -13,7 +13,7 @@ public class SoftAssertionsTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com/";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
         Configuration.timeout = 5000; // default 4000
     }
 
